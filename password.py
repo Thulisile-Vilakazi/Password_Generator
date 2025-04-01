e8f943be90899af97eefa85d112559284745f492
Thulisile-Vilakazi <thuliveronicavilakazi9@gmail.com>
2025-04-01 20:44:15 +0200

    Import random

--- password.py
+++ password.py
@@ -1,3 +1,4 @@
+import random
 letters = [
     'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
     'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
@@ -13,11 +14,12 @@ symbols = [
 
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
-password = ""
+password = []
 
 print("Let's generate a random password for you")
 num_letters = input("How many letters should the password have? ")
 num_symbols = input("How many symbols should the password have? ")
 num_nums = input("How many numbers should the password have? ")
 
-for char in range(1, num_letters + 1)
+for char in range(1, num_letters):
+    password.append(random)
